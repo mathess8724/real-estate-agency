f916ecaa1a4280c84de7de0d02100209b0bed5ba
mathess8724 <67884739+mathess8724@users.noreply.github.com>
2021-09-09 09:59:38 +0100

    cleaning

--- src/components/home/modals/Modal.js
+++ src/components/home/modals/Modal.js
@@ -4,26 +4,19 @@ import './Modal.scss'
 
 function Modal(modalInfos){
         
-    let id = 0
-    
-    
+    let id = 0   
 
     function changeImg(direction){
         console.log('carroussel!')
         direction === 'right' ? id ++ : id = id -1 
         console.log('new id is ' + id)
         
-    }
-        //console.log(modalInfos.imgGalerie[modalInfos.imgSrc].imgSrc)
-    return(
-
-                 
+    }       
+    return(                 
         <div className="modalBody">  
         <span className="back"></span>          
-            <div className="modalContainer">
-                
-            <h1>{modalInfos.name}</h1>
-           
+            <div className="modalContainer">                
+            <h1>{modalInfos.name}</h1>           
             <div className="modalImg">
                 <i onClick={ () => changeImg('')} className='fas fa-chevron-left imgArrows'></i>
                 <img  style={{height:'100%', width:'100%'}} src={modalInfos.imgGalerie[id].imgSrc} alt='propertie Image'/>
